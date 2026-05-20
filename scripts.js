96411ee036e1fe93442f1e0c771d8a1dd4da7ff3
romo509 <romo509@gmail.com>
2026-05-19 18:17:17 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById('mobile-menu');
-const navList = document.querySelector('.nav-list');
-
-menu.addEventListener('click', () => {
-  navList.classList.toggle('active');
-});
